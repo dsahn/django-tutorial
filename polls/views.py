e03cd5750da331b77b52156f028c0132be689a60
dsahn <ads50112@gmail.com>
2021-01-31 02:46:27 +0900

    render 적용

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,13 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
             'latest_question_list' : latest_question_list,
             }
-    return HttpResponse(template.render(context,request))
+    return render(request, 'polls/index.html', context)
     # 안씀 : template 을 이용하기위해
     # output = ', '.join([q.question_text for q in latest_question_list])
 
